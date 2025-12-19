e91aa69aa4bfeb8e66ec78ed7a1511d581b31c70
AsadFahimTEC <fah485434@gmail.com>
2025-12-19 12:11:25 +0600

    join all issue solve

--- PostgreSQL-b5-module19-practice-tasks/fourththree.sql
+++ PostgreSQL-b5-module19-practice-tasks/fourththree.sql
@@ -14,8 +14,33 @@ inner join courses as c on e.course_id = c.course_id
 
 -- Display all students and their enrolled courses.
 -- Include students who have not enrolled in any course using a LEFT JOIN.
+SELECT
+  CONCAT(s.first_name, ' ', s.last_name) AS student_name,
+  COALESCE(c.course_title, 'Not Enrolled') AS course_title,
+  e.paid_amount
+FROM students s
+LEFT JOIN enrollments e ON s.student_id = e.student_id
+LEFT JOIN courses c ON e.course_id = c.course_id
+ORDER BY s.first_name, s.last_name;
 
 -- Display all courses and their enrolled students.
 -- Include courses that have no enrollments using a RIGHT JOIN.
+  SELECT
+  c.course_title,
+  CONCAT(s.first_name, ' ', s.last_name) AS student_name,
+  e.paid_amount
+FROM enrollments e
+RIGHT JOIN courses c ON e.course_id = c.course_id
+LEFT JOIN students s ON e.student_id = s.student_id
+ORDER BY c.course_title;
 
 -- Display all students and all courses, even if there is no matching enrollment, using a FULL JOIN
+
+SELECT
+  CONCAT(s.first_name, ' ', s.last_name) AS student_name,
+  c.course_title,
+  e.paid_amount
+FROM students s
+FULL JOIN enrollments e ON s.student_id = e.student_id
+FULL JOIN courses c ON e.course_id = c.course_id
+ORDER BY student_name, c.course_title;
